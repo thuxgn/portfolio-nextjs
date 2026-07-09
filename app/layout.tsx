114db6690f9503ed4e5a970ea0883458db2d1575
thuxgn <thucndt.huce@gmail.com>
2026-07-09 11:19:05 +0700

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://thuxgn.site"),
   title: "thuxgn",
   description: "Portfolio",
   authors: [{ name: "thuxgn" }],
